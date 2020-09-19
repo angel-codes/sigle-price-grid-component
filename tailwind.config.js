1e36a619bad191e7639fffc82cd4a49ca5df352f
Angel Martinez <angel.codes139@gmail.com>
2020-09-19 17:47:20 -0500

    :wrench: adding custom colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bright-yellow': '#c0df34',
+        'light-gray': '#e5eff5',
+        'grayish-blue': '#98a6bd'
+      }
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
